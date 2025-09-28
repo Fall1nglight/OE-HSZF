2f98a04619511c11d0b371a08887bf95564a8bf4
Fall1ngLight <keruloszabolcs1@gmail.com>
2025-09-28 20:22:47 +0200

    ➕ add: linq

---
+++ Linq/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Xml.Schema;
+
+namespace Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Student> groupA = new List<Student>()
+        {
+            new Student("John", 180, 3),
+            new Student("Mark", 191, 4),
+            new Student("Josh", 162, 5),
+        };
+
+        List<Student> groupB = new List<Student>()
+        {
+            new Student("John", 180, 3),
+            new Student("David", 159, 3),
+            new Student("Milan", 167, 4),
+        };
+
+        // Első feladat
+        var metszet = groupA.Intersect(groupB);
+        var unio = groupA.Union(groupB);
+        var kulonbseg = groupA.Except(groupB);
+
+        // Második feladat
+        var students = unio.ToList();
+        var hasHeigherThan180 = students.Any(s => s.Height > 180);
+        var noneFailed = students.All(s => s.Mark > 1);
+        var betterThanMark3 = students.Where(s => s.Mark > 3);
+        var heigherThan170Count = students.Count(s => s.Height > 170);
+
+        // var bestMark = students.OrderBy(s => s.Mark).Select(s => s.Mark).First();
+        var bestMark = students.Max(s => s.Mark);
+        var bestStudents = students.Where(s => s.Mark == bestMark);
+
+        var top3Students = students.OrderByDescending(s => s.Mark).Take(3);
+        var top3StudentNames = students.OrderByDescending(s => s.Name).Take(3).Select(s => s.Name);
+    }
+}
---
+++ Linq/Student.cs
@@ -0,0 +1,43 @@
+﻿namespace Linq;
+
+public class Student
+{
+    // fields
+    private string _name;
+    private int _height;
+    private int _mark;
+
+    // properties
+    public string Name => _name;
+
+    public int Height => _height;
+
+    public int Mark => _mark;
+
+    // constructors
+    public Student(string name, int height, int mark)
+    {
+        _name = name;
+        _height = height;
+        _mark = mark;
+    }
+
+    // methods
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(this, obj))
+            return true;
+
+        var other = obj as Student;
+
+        if (other == null)
+            throw new ArgumentNullException(nameof(obj), "Cannot compare null values.");
+
+        return Name.Equals(other.Name) && Height == other.Height && Mark == other.Mark;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name, Height, Mark);
+    }
+}
